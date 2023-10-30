15f1364f647942d54a03f991daf6559610dfa576
GabrielWiel <gabrielwielogorski@outlook.com>
2023-10-30 17:56:25 +0000

    Fixed

--- Prime_Numbers.py
+++ Prime_Numbers.py
@@ -1,15 +1,33 @@
-start = input("What is the first number of the range1 that you want to know the prime numbers of: ")
-end = input("What is the second number of the range that you want to know the prime numbers of: ")
-end = int(end) + 1
-start = int(start) - 1
-for num in range(int(start),int(end)):
-    prime = True
-    for i in range(2,num):
-        if (num%i==0):
-            prime = False
-    if prime:
-       print (num)
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
-file = open('test.txt', 'a')
-file.write(str(num))
-file.close()
+def find_primes_in_range(start, end):
+    primes = [str(num) for num in range(start, end + 1) if is_prime(num)]
+    return primes
+
+try:
+    start = int(input("Enter the starting number: "))
+    end = int(input("Enter the ending number: "))
+
+    prime_numbers = find_primes_in_range(start, end)
+
+    with open("test.txt", "w") as file:
+        file.write("\n".join(prime_numbers))
+    print(prime_numbers)
+    print(f"Prime numbers in the range {start} to {end} have been saved to 'test.txt'.")
+
+except ValueError:
+    print("Please enter valid integer values for the range.")
+except IOError:
+    print("An error occurred while writing to the file.")
